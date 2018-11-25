eb58278838af625023b8a505e7c2bc3e1c1bde44
chenchen <chkunshuang@gmail.com>
2018-11-25 22:31:37 +0800

    添加ts支持

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React, {Component} from 'react';
+import * as React from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 const instructions = Platform.select({
@@ -16,11 +16,11 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
